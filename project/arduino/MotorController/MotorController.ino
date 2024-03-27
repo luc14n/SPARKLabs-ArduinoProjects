d50275c1007711d5240aa5bc0d52f391a3434a4c
Jonathan Eanes <jdeanes0@frostburg.edu>
2024-03-27 18:50:33 -0400

    Finished untested line following code for light sensors on breadboard

--- project/arduino/MotorController/MotorController.ino
+++ project/arduino/MotorController/MotorController.ino
@@ -9,12 +9,12 @@ int motor2speed = 10; //ENA pin
 // Input Cluster: 
 int lightsensRIGHTpin = A3; // ANALOG
 int lightsensLEFTpin = A2; // ANALOG
-// -> Digital Inputs:
-int potentiometerpin = 3; // DIGITAL
+int potentiometerpin = A1; // ANALOG
 int startButtonpin = 6; // DIGITAL
 
 // Output Cluster:
 int signalLEDpin = 7; // DIGITAL
+const int PRESET_SENSITIVITY = 25;
 
 void setup() {
   Serial.begin(9600);
@@ -41,28 +41,62 @@ void setup() {
 
   // While loop to hold before button press
   digitalWrite(signalLEDpin, HIGH);
+
+  config();
+}
+
+void config() {
   int controlButtonStatus = LOW;
+  int adjLightReadRight;
+  int adjLightReadLeft;
+  int adjPoten;
   while (true) {
-    delay(10);
+    delay(300);
     controlButtonStatus = digitalRead(startButtonpin);
     if (controlButtonStatus == HIGH) {
       digitalWrite(signalLEDpin, LOW);
-      loop();
+      delay(1000);
+      break;
     }
-    if (analogRead(lightsensLEFTpin) || analogRead(lightsensRIGHTpin)) {
+    if (adjLightReadRight < PRESET_SENSITIVITY || adjLightReadLeft < PRESET_SENSITIVITY ) {
+      digitalWrite(signalLEDpin, HIGH);
+    } else {
+      digitalWrite(signalLEDpin, LOW);
+    }
+    
+    adjPoten = map(analogRead(potentiometerpin), 0, 1023, 500, 5000);
+    adjLightReadRight = map((long) (analogRead(lightsensRIGHTpin) * (adjPoten / 100)), 0, 1023, 1, 100);
+    adjLightReadLeft = map((long) (analogRead(lightsensLEFTpin) * (adjPoten / 100)), 0, 1023, 1, 100);
+    // Serial.print(adjLightReadRight);
+    // Serial.print("\n");
+    // Serial.print(adjLightReadLeft);
+    // Serial.print("\n");
+    Serial.print("poten: ");
+    Serial.print(adjPoten);
+    Serial.print("\n");
+    Serial.print("right: ");
+    Serial.print(adjLightReadRight);
+    Serial.print("\n");
+    Serial.print("left: ");
+    Serial.print(adjLightReadLeft);
+    Serial.print("\n");
 
-    }
-    // Write potentiometer to light sensors
+
     //digitalWrite();
 
   }
 }
 
+int sensitivityScale;
+int lightRight;
+int lightLeft;
+
 void loop() {
   // put your main code here, to run repeatedly:
+
   //Controlling speed (0  = off and 255 = max speed):
-  analogWrite(motor1speed, 255); 
-  analogWrite(motor2speed, 255); 
+  // analogWrite(motor1speed, 255); 
+  // analogWrite(motor2speed, 255); 
 
   // Both Run the same direction set up like this. WARNING flipping motors will flip directions.
   digitalWrite(motor1pin1,  HIGH);
@@ -70,5 +104,42 @@ void loop() {
 
   digitalWrite(motor2pin1,  HIGH);
   digitalWrite(motor2pin2, LOW);
+
+  delay(10);
+
+  sensitivityScale = map(analogRead(potentiometerpin), 0, 1023, 500, 5000);
+  lightRight = map((long) (analogRead(lightsensRIGHTpin) * (sensitivityScale / 100)), 0, 1023, 1, 100);
+  lightLeft = map((long) (analogRead(lightsensLEFTpin) * (sensitivityScale / 100)), 0, 1023, 1, 100);
+
+  if (lightRight < PRESET_SENSITIVITY && lightLeft < PRESET_SENSITIVITY ) {
+    digitalWrite(signalLEDpin, HIGH);
+  } else {
+    digitalWrite(signalLEDpin, LOW);
+  }
+
+  if (digitalRead(startButtonpin) == HIGH) {
+    digitalWrite(motor1pin1, LOW);
+    digitalWrite(motor1pin2, LOW);
+
+    digitalWrite(motor2pin1, LOW);
+    digitalWrite(motor2pin2, LOW);
+
+    config();
+  }
+
+  if (lightRight > PRESET_SENSITIVITY && lightLeft > PRESET_SENSITIVITY) { // 0 - 0
+    adjustSpeed(50, 100);
+  } else if (lightRight > PRESET_SENSITIVITY && lightLeft <= PRESET_SENSITIVITY) { // 0 - 1
+    adjustSpeed(100, 30);
+  } else if (lightRight <= PRESET_SENSITIVITY && lightRight > PRESET_SENSITIVITY) { // 1 - 0
+    adjustSpeed(75, 100);
+  } else { // 1 - 1
+    adjustSpeed(100, 50);
+  }
+
 }
 
+void adjustSpeed(int left, int right) {
+  analogWrite(motor1speed, map(right, 0, 100, 50, 255)); 
+  analogWrite(motor2speed, map(left, 0, 100, 50, 255)); 
+}
